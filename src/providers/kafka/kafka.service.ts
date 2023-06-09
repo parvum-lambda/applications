118e59004460c9c764af9a3fe63f180f4bf9e952
Maicon Oliveira <maicon.natan-contractor@procore.com>
2023-06-09 12:16:35 -0300

    fix: kafka service no longer using dummy config;

--- src/providers/kafka/kafka.service.ts
+++ src/providers/kafka/kafka.service.ts
@@ -7,11 +7,7 @@ export class KafkaService {
   private readonly kafka: Kafka;
   private readonly consumers: Consumer[];
   public constructor() {
-    //  this.kafka = new Kafka({ brokers: process.env.KAFKA_BROKERS.split(',') });
-    this.kafka = new Kafka({
-      clientId: 'aaa',
-      brokers: ['128.4.0.2:9092'],
-    });
+    this.kafka = new Kafka({ brokers: process.env.KAFKA_BROKERS.split(',') });
   }
 
   public async publish<T extends BaseData>(event: BaseEvent<T>) {
